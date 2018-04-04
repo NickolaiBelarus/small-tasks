1e91d766d6a76a84692d9e1c22b880760c3b63bb
Nick <chebotar.nickolai@gmail.com>
2018-04-04 19:14:10 +0300

    destroy all & create new

--- app.js
+++ app.js
@@ -1,78 +1,60 @@
-var express = require('express');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var app = express();
-var multer = require('multer')
-var constants = require('constants');
-var constant = require('./config/constants');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const account = require('./routes/account');
+const id = require('./routes/id');
 
+const app = express();
 
-var port = process.env.PORT || 8042;
-var MongoClient = require('mongodb').MongoClient;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash = require('connect-flash');
-var path = require('path');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-var morgan = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var dateFormat = require('dateformat');
-var now = new Date();
-
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-
-/***************Mongodb configuratrion********************/
-var mongoose = require('mongoose');
-var configDB = require('./config/database.js');
-//configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
-
-
-require('./config/passport')(passport); // pass passport for configuration
-
-//set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-//app.use(bodyParser()); // get information from html forms
-
-//view engine setup
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'app/views'));
-app.set('view engine', 'ejs');
-//app.set('view engine', 'ejs'); // set up ejs for templating
-
-
-//required for passport
-//app.use(session({ secret: 'iloveyoudear...' })); // session secret
-
-app.use(session({
-    secret: 'I Love India...',
-    resave: true,
-    saveUninitialized: true
+app.use(bodyParser.urlencoded({
+    extended: false
 }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
-
-// routes ======================================================================
-require('./config/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
-
-
-//launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
-
-//catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    res.status(404).render('404', {title: "Sorry, page not found", session: req.sessionbo});
+app.use('/', index);
+app.use('/users', users);
+app.use('/account', account);
+app.use('/account/:id', id);
+app.use('/account/:id', (req, res, next) => {
+    console.log('Request URL:', req.originalUrl);
+    next();
+}, (req, res, next) => {
+    console.log('Request Type:', req.method);
+    next();
+}, (req, res, next) => {
+    console.log('ID:', req.params.id);
+    next();
 });
 
-app.use(function (req, res, next) {
-    res.status(500).render('404', {title: "Sorry, page not found"});
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
-exports = module.exports = app;
+
+// error handler
+app.use((err, req, res) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
+// dsgfhdgfh
